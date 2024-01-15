e12a80f1d69d38672ea8caf4875724cb4dc3d079
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-15 20:47:46 +0300

    [Chore]: Refactor Components

--- 0x01-part_one/courseinfo/src/App.jsx
+++ 0x01-part_one/courseinfo/src/App.jsx
@@ -24,27 +24,34 @@ const Content = (props) => {
 const Total = (props) => {
   return <p>Number of exercises {props.total}</p>;
 };
+
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <>
       <Header course={course} />
       <Content
-        part1={part1}
-        exercise1={exercises1}
-        part2={part2}
-        exercise2={exercises2}
-        part3={part3}
-        exercise3={exercises3}
+        part1={part1.name}
+        exercise1={part1.exercises}
+        part2={part2.name}
+        exercise2={part2.exercises}
+        part3={part3.name}
+        exercise3={part3.exercises}
       />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </>
   );
 };
