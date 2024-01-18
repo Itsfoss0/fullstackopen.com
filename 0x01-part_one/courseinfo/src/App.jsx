47e13d78592f29d54e3aedcab235c10ffef12fc4
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-18 19:50:49 +0300

    [Chore]: Refactor components to use best practices

--- 0x01-part_one/courseinfo/src/App.jsx
+++ 0x01-part_one/courseinfo/src/App.jsx
@@ -1,3 +1,41 @@
+/* eslint-disable no-unused-vars */
+
+import { useState } from 'react';
+
+const Button = ({ onSmash, text }) => {
+  return <button onClick={onSmash}>{text}</button>
+};
+
+const Counter = ({ count }) => {
+  const [number, setNumber] = useState(count)
+
+  const incrementNumber = () => {
+    setNumber(number + 1)
+  }
+
+  const decrementNumber = () => {
+    if (number > 0) {
+      setNumber(number - 1)
+    }
+    else {
+      setNumber(0)
+    }
+  }
+
+  const reset = () => {
+    if (number != 0) {
+      setNumber(0);
+    }
+  }
+  return (<>
+    <div>{number}</div>
+    <Button onSmash={incrementNumber} text="+" />
+    <Button onSmash={decrementNumber} text="-" />
+    <Button onSmash={reset} text="Reset" />
+  </>
+  );
+}
+
 const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
@@ -25,35 +63,83 @@ const Total = (props) => {
   return <p>Number of exercises {props.total}</p>;
 };
 
+const Person = ({ name, age }) => {
+  return (<>
+    <div> Hi there {name}, good to see you grown to {age} </div>
+  </>);
+};
+
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
 
   return (
     <>
-      <Header course={course} />
+      <Header course={course.name} />
       <Content
-        part1={part1.name}
-        exercise1={part1.exercises}
-        part2={part2.name}
-        exercise2={part2.exercises}
-        part3={part3.name}
-        exercise3={part3.exercises}
+        part1={course.parts[0].name}
+        exercise1={course.parts[0].exercises}
+        part2={course.parts[1].name}
+        exercise2={course.parts[1].exercises}
+        part3={course.parts[2].name}
+        exercise3={course.parts[2].exercises}
       />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Total
+        total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+      />
+      <Person name="John" age={33} />
+      <Counter count={21} />
     </>
   );
+};
+
+const History = ( { clicks }) => {
+  if (clicks.length === 0 ){
+    return <div>Click the Buttons to use the application</div>
+  } else {
+    return <div>{clicks.join('-')}</div>
+  }
+}
+export const Mirror = ({ l, r }) => {
+
+  const [clicks, setClicks] = useState({ left: l, right: r })
+  const [allClicks, setallClicks] = useState([])
+  const [total, setTotal] = useState(clicks.left + clicks.right)
+
+  const incrementLeftNum = () => {
+    setClicks({ ...clicks, left: clicks.left += 1 })
+    setallClicks(allClicks.concat('L'))
+    setTotal(clicks.left + clicks.right)
+  }
+  
+  const incrementRightNum = () => {
+    setClicks({ ...clicks, right: clicks.right += 1 })
+    setallClicks(allClicks.concat('R'))
+    setTotal(clicks.left + clicks.right)
+
+  }
+  return (<>
+    <div> Left at {clicks.left}, Right Num at {clicks.right} </div>
+    <History clicks={allClicks}/>
+    <div>All Buttons clicked {total} times</div>
+    <Button onSmash={incrementLeftNum} text="Left" />
+    <Button onSmash={incrementRightNum} text="Right" />
+  </>);
 };
 
 export default App;
--- 0x01-part_one/courseinfo/src/main.jsx
+++ 0x01-part_one/courseinfo/src/main.jsx
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App, { Mirror } from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<div><App /> <Mirror l={12} r={15} /></div>);
