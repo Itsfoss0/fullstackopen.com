dd00e63a2d27fa4f221e801de630702e0f8fa2f4
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-18 21:35:38 +0300

    [Fix]: Fix Statistic component
    Refactor the component to render the correct data, based on the state in the App component

--- 0x01-part_one/unicafe/src/App.jsx
+++ 0x01-part_one/unicafe/src/App.jsx
@@ -3,44 +3,68 @@
 import { useState } from 'react';
 
 const Header = ({ title }) => {
-  return <h1>{title}</h1>
-}
+  return <h1>{title}</h1>;
+};
 
-const Statistic = ({ statName, statVal }) => {
+const StatisticLine = ({ statName, statVal }) => {
   return (<div>{statName}: {statVal} </div>);
-}
+};
+
+const Statistic = ({ props }) => {
+  return (
+    <>
+      <StatisticLine statName='Good' statVal={props.stats.good} />
+      <StatisticLine statName='Bad' statVal={props.stats.bad} />
+      <StatisticLine statName='Neutral' statVal={props.stats.neutral} />
+      <StatisticLine statName='All' statVal={props.totalFB} />
+      <StatisticLine statName='Average' statVal={props.average} />
+      <StatisticLine statName='Positive' statVal={props.positive + '%'} />
+    </>
+  );
+};
+
 const Button = ({ feedBack, text, value }) => {
-  return <button onClick={() => feedBack(value)}>{text}</button>
-}
+  return <button onClick={() => feedBack(value)}>{text}</button>;
+};
 
 const App = () => {
-  const [stats, setStats] = useState({ good: 0, bad: 0, neutral: 0 })
-  const [totalFB, setTotalFB] = useState(0)
-  const [positive, setPositive] = useState(0)
-  const [average, setAverage] = useState(0)
+  const [stats, setStats] = useState({ good: 0, bad: 0, neutral: 0 });
+  const [totalFB, setTotalFB] = useState(0);
+  const [positive, setPositive] = useState(0);
+  const [average, setAverage] = useState(0);
 
   const updateFeedback = (value) => {
-    setStats({ ...stats, [value]: stats[value] + 1 })
-    setTotalFB(totalFB + 1)
+    setStats({ ...stats, [value]: stats[value] + 1 });
+    setTotalFB(totalFB + 1);
     const calculatedAverage = (stats.good - stats.bad) / totalFB;
     setAverage(isNaN(calculatedAverage) ? 0 : calculatedAverage);
-    const positiveFB = (stats.good / totalFB) * 100
+    const positiveFB = (stats.good / totalFB) * 100;
     setPositive(isNaN(positive) ? 0 : positiveFB);
-  }
+  };
 
-  return (<>
-    <Header title="Unicafe Feedback Page" />
-    <Button feedBack={updateFeedback} text="Good" value="good" />
-    <Button feedBack={updateFeedback} text="Neutral" value="neutral" />
-    <Button feedBack={updateFeedback} text="Bad" value="bad" />
-    <h2>Statistics</h2>
-    <Statistic statName="Good" statVal={stats.good}/>
-    <Statistic statName="Bad" statVal={stats.bad}/>
-    <Statistic statName="Neutral" statVal={stats.neutral}/>
-    <Statistic statName="All" statVal={totalFB}/>
-    <Statistic statName="Average" statVal={average}/>
-    <Statistic statName="Positive" statVal={positive + "%"}/>
-  </>);
-}
+  const stateInfo = {
+    stats,
+    totalFB,
+    positive,
+    average
+  };
+  return (
+    <>
+      <Header title='Unicafe Feedback Page' />
+      <Button feedBack={updateFeedback} text='Good' value='good' />
+      <Button feedBack={updateFeedback} text='Neutral' value='neutral' />
+      <Button feedBack={updateFeedback} text='Bad' value='bad' />
+      <h2>Statistics</h2>
+      {totalFB > 0
+        ? (
+          <Statistic props={stateInfo} />
+        )
+        : (
+          <div>No feedback given</div>
+        )}
+
+    </>
+  );
+};
 
 export default App;
