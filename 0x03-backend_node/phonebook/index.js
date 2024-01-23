44cf9d62b4f5a5318cbad66d82920432d7a86fe5
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-23 22:30:48 +0300

    lint: properly lint the project

--- 0x03-backend_node/phonebook/index.js
+++ 0x03-backend_node/phonebook/index.js
@@ -1,35 +1,35 @@
 #!/usr/bin/env node
 
-const express = require("express");
-const cors = require('cors')
-const { undefinedRouteHandler } = require("./middleware/errorHandlers");
-const { loggerMidleware } = require("./middleware/logging");
+const express = require('express');
+const cors = require('cors');
+const { undefinedRouteHandler } = require('./middleware/errorHandlers');
+const { loggerMidleware } = require('./middleware/logging');
 
-const { generateId } = require("./utils/utils");
+const { generateId } = require('./utils/utils');
 
 const PORT = process.env.PORT || 1337;
 
 let persons = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456'
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345'
   },
   {
     id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
 ];
 
 const app = express();
@@ -38,7 +38,7 @@ app.use(cors());
 app.use(express.json());
 app.use(loggerMidleware);
 
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   const date = new Date().toString();
   const personsCount = persons.length;
   response.send(
@@ -46,30 +46,30 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   return response.json(persons);
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const personId = Number(request.params.id);
   const person = persons.find((person) => person.id === personId);
   if (person) {
     return response.json(person);
   }
-  return response.status(404).json({ error: "No person found" });
+  return response.status(404).json({ error: 'No person found' });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const personId = Number(request.params.id);
   const personToDelete = persons.find((person) => person.id === personId);
   if (personToDelete) {
     persons = persons.filter((person) => person.id !== personToDelete.id);
-    return response.json(personToDelete)
+    return response.json(personToDelete);
   }
-  return response.status(404).json({ error: "person not found" });
+  return response.status(404).json({ error: 'person not found' });
 });
 
-app.post("/api/persons", (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body;
   if (body.name && body.number) {
     const person = persons.find((psn) => psn.name === body.name);
@@ -77,18 +77,18 @@ app.post("/api/persons", (request, response) => {
       const newPerson = {
         name: body.name,
         number: body.number,
-        id: generateId(persons),
+        id: generateId(persons)
       };
       persons = persons.concat(newPerson);
       return response.json(newPerson);
     }
     return response
       .status(409)
-      .json({ error: "Person with that name already exists" });
+      .json({ error: 'Person with that name already exists' });
   }
   return response
     .status(400)
-    .json({ error: "name or number not specified in the reqeust" });
+    .json({ error: 'name or number not specified in the reqeust' });
 });
 
 app.use(undefinedRouteHandler);
