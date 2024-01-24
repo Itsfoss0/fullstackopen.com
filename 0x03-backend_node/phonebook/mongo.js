6e72a1efd483d03edfb735d6ee3b473723f4ccf8
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-24 17:10:20 +0300

    chore: add mongodb service

---
+++ 0x03-backend_node/phonebook/mongo.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+/* DB service for the API */
+
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.argv[2];
+
+const connection = mongoose
+  .connect(MONGO_URI)
+  .catch((err) => console.error(err.message));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number
+});
+
+const Person = mongoose.model('person', personSchema);
+
+if (process.argv.length >= 5) {
+  const newPerson = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  });
+  newPerson.save().then(() => {
+    console.log(
+      `Added ${newPerson.name} number ${newPerson.number} to phonebook`
+    );
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log('Phonebook:');
+    result.forEach((res) => console.log(`${res.name} ${res.number}`));
+    mongoose.connection.close();
+  });
+}
