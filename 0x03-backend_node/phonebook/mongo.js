5a7a2927662ea694915cfa4471b9764eb7d7dde7
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-26 03:24:20 +0300

    chore: lint the db service

--- 0x03-backend_node/phonebook/mongo.js
+++ 0x03-backend_node/phonebook/mongo.js
@@ -2,26 +2,24 @@
 
 /* DB service for the API */
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const PASSWORD = process.argv[2];
 const MONGO_URI = `mongodb+srv://itsfoss:${PASSWORD}@fullstackopen.9koiyy6.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose
-  .connect(MONGO_URI)
-  .catch((err) => console.error(err.message));
+mongoose.connect(MONGO_URI).catch((err) => console.error(err.message));
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: Number
+  number: Number,
 });
 
-const Person = mongoose.model('person', personSchema);
+const Person = mongoose.model("person", personSchema);
 
 if (process.argv.length >= 5) {
   const newPerson = new Person({
     name: process.argv[3],
-    number: process.argv[4]
+    number: process.argv[4],
   });
   newPerson.save().then(() => {
     console.log(
@@ -31,7 +29,7 @@ if (process.argv.length >= 5) {
   });
 } else if (process.argv.length === 3) {
   Person.find({}).then((result) => {
-    console.log('Phonebook:');
+    console.log("Phonebook:");
     result.forEach((res) => console.log(`${res.name} ${res.number}`));
     mongoose.connection.close();
   });
