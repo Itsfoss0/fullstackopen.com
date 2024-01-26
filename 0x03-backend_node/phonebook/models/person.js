b9ad0196c80239a3fed43c258e4fb6289dfe3cb8
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-26 03:25:34 +0300

    chore: lint models

--- 0x03-backend_node/phonebook/models/person.js
+++ 0x03-backend_node/phonebook/models/person.js
@@ -1,29 +1,29 @@
 #!/usr/bin/env node
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI)
-  .then(() => console.log('Connected succesfully'))
+  .then(() => console.log("Connected succesfully"))
   .catch((err) =>
     console.log(`Error connecting to database\n ${err.message} `)
   );
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String
+  number: String,
 });
 
-personSchema.set('toJSON', {
+personSchema.set("toJSON", {
   transform: (document, newObject) => {
     newObject.id = newObject._id.toString();
     delete newObject._id;
     delete newObject.__v;
-  }
+  },
 });
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model("Person", personSchema);
 
 module.exports = Person;
