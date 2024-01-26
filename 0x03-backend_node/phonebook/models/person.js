7d0020fcecfeeaa17e35086095f9c73bce84caea
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-26 03:16:29 +0300

    chore: add schema and data models for the api

---
+++ 0x03-backend_node/phonebook/models/person.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log('Connected succesfully'))
+  .catch((err) =>
+    console.log(`Error connecting to database\n ${err.message} `)
+  );
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+personSchema.set('toJSON', {
+  transform: (document, newObject) => {
+    newObject.id = newObject._id.toString();
+    delete newObject._id;
+    delete newObject.__v;
+  }
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+module.exports = Person;
