3a94881a180e1df0b9c86037a4073a1b5e5abe14
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-28 23:12:52 +0300

    test: add unittest for the utility functions

---
+++ 0x04-authentication_testing/bloglist/tests/helper.test.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+/* unittest for the utilty helper functions */
+
+const { dummy, totalLikes, favoriteBlog } = require("../utils/list_helper");
+
+test("dummy returns one", () => {
+  const blogs = [];
+
+  const result = dummy(blogs);
+  expect(result).toBe(1);
+});
+
+describe("Total Likes", () => {
+  test("of empty list is zero", () => {
+    expect(totalLikes([])).toBe(0);
+  });
+
+  test("when list has only one blog, equals to the likes of that", () => {
+    expect(totalLikes([{ likes: 3 }])).toBe(3);
+  });
+
+  test("of a bigger blog list are culculated right", () => {
+    const blogs = [{ likes: 8 }, { likes: 4 }, { likes: 4 }];
+    expect(totalLikes(blogs)).toBe(16);
+  });
+});
+
+describe("Favorite Blog", () => {
+  test("when list has only one blog, equals to the only one", () => {
+    const blogs = [{ likes: 3 }];
+    expect(favoriteBlog(blogs)).toEqual(blogs[0]);
+  });
+
+  test("when list has mulitple blogs, equals the correct one", () => {
+    const blogs = [{ likes: 3 }, { likes: 5 }];
+    expect(favoriteBlog(blogs)).toEqual(blogs[1]);
+  });
+
+  test("when list has mulitple blogs with the same likes, equals the first one", () => {
+    const blogs = [{ likes: 3 }, { likes: 5 }, { likes: 5 }];
+    expect(favoriteBlog(blogs)).toEqual(blogs[1]);
+  });
+
+  test("when list doesn't have any blogs, is none", () => {
+    const blogs = [];
+    expect(favoriteBlog(blogs)).toEqual(blogs[0]);
+  });
+});
