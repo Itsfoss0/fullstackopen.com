0fb2bb256fcda556b62fe76b54ce14d274112a52
Itsfoss0 <com01321@uoeld.ac.ke>
2024-01-30 20:19:08 +0300

    chore: add helper scripts

--- 0x04-authentication_testing/bloglist/script.sh
+++ 0x04-authentication_testing/bloglist/script.sh
@@ -5,5 +5,6 @@
 # before running `npm start`
 # WARNING: Not to be used for production
 
+export NODE_ENV=prod
 export MONGO_URI="mongodb://127.0.0.1/bloglist"
 export PORT=1337
---
+++ 0x04-authentication_testing/bloglist/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# setup testing environment 
+
+export TEST_DB="mongodb://127.0.0.1/bloglisttest"
+export PORT=1337
