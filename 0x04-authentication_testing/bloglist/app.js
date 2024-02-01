8a479073a1870f627c098467fa3242f5dccb9352
Itsfoss0 <com01321@uoeld.ac.ke>
2024-02-02 00:43:52 +0300

    feat: support token authentication

--- 0x04-authentication_testing/bloglist/app.js
+++ 0x04-authentication_testing/bloglist/app.js
@@ -9,6 +9,7 @@ const morgan = require("morgan");
 require("express-async-errors");
 const blogRouter = require("./controllers/blogs");
 const userRouter = require("./controllers/users");
+const authRouter = require("./controllers/auth");
 const { undefinedRouteHandler } = require("./middleware/errors");
 const { MONGO_URI } = require("./config/config");
 const { info, error } = require("./utils/logger");
@@ -20,6 +21,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
 app.use(undefinedRouteHandler);
 
 if (process.env.NODE_ENV !== "test") {
