522d18d9bf7d13f22993f77bb8fe5663b692169c
Itsfoss0 <com01321@uoeld.ac.ke>
2024-02-02 16:31:24 +0300

    lint: fix semistandard linter issues

--- 0x04-authentication_testing/bloglist/config/config.js
+++ 0x04-authentication_testing/bloglist/config/config.js
@@ -3,11 +3,11 @@
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const NODE_ENV = process.env.NODE_ENV;
 const MONGO_URI =
-  NODE_ENV === "test" ? process.env.TEST_DB : process.env.MONGO_URI;
+  NODE_ENV === 'test' ? process.env.TEST_DB : process.env.MONGO_URI;
 const PORT = process.env.PORT;
 
 module.exports = {
   MONGO_URI,
   PORT,
-  JWT_SECRET_KEY,
+  JWT_SECRET_KEY
 };
