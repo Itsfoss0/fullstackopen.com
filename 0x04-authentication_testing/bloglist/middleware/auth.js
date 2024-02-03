2e550b9a740a0e9d532366dde63a0f664998fa0c
Itsfoss0 <akhonyaebenezer@gmail.com>
2024-02-03 10:38:25 +0300

    lint: properly format the auth middleware

--- 0x04-authentication_testing/bloglist/middleware/auth.js
+++ 0x04-authentication_testing/bloglist/middleware/auth.js
@@ -2,8 +2,8 @@
 /* extract token from reqeust headers and set them as the request.token */
 
 const tokenExtractor = (request, response, next) => {
-  if (request.method === "GET") return next();
-  const auth = request.get("authorization").replace("Bearer ", "");
+  if (request.method === 'GET') return next();
+  const auth = request.get('authorization').replace('Bearer ', '');
   request.token = auth;
   console.log(request.token);
   return next();
