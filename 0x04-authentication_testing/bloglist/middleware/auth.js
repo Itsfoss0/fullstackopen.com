7c47eec304e2bb62ec29f00a43a9771a375f7baa
Itsfoss0 <akhonyaebenezer@gmail.com>
2024-02-03 12:12:30 +0300

    chore: add middleware to extract user from request

--- 0x04-authentication_testing/bloglist/middleware/auth.js
+++ 0x04-authentication_testing/bloglist/middleware/auth.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
-/* extract token from reqeust headers and set them as the request.token */
+
+/* extract token from the authorization header
+ * and determine the user from the token,
+ * assign the user to the request.user prop
+ * while at the same time assign the token
+ * to request.token for easier auth handling
+ * on routes that require authorization
+ * the tokenExtractor middlware has to be applied
+ * before the userExtractor middleware
+ * Example usage:
+ * router.use(tokenExtractor)
+ * router.use(userExtractor)
+ */
+
+const { JWT_SECRET_KEY } = require('./../config/config');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const tokenExtractor = (request, response, next) => {
   if (request.method === 'GET') return next();
@@ -9,4 +25,24 @@ const tokenExtractor = (request, response, next) => {
   return next();
 };
 
-module.exports = tokenExtractor;
+const userExtractor = async (request, response, next) => {
+  // a get request doesn't have a token prop
+  // it'd be useless to try and get the user for it
+  if (request.method === 'GET') return next();
+  try {
+    const decodedToken = jwt.verify(request.token, JWT_SECRET_KEY);
+    const user = await User.findById(decodedToken.userId);
+    if (user) {
+      request.user = user;
+      return next();
+    }
+    request.user = null;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
+module.exports = {
+  tokenExtractor,
+  userExtractor
+};
