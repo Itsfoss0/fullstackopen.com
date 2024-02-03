2a6aac70d9503677bd539966acd12b9953d5d27c
Itsfoss0 <akhonyaebenezer@gmail.com>
2024-02-03 12:12:11 +0300

    feat: restrict modifying blog to the owner

--- 0x04-authentication_testing/bloglist/controllers/blogs.js
+++ 0x04-authentication_testing/bloglist/controllers/blogs.js
@@ -2,14 +2,13 @@
 
 /* Router for the api/blogs endpoint */
 
-const jwt = require('jsonwebtoken');
-const tokenExtractor = require('../middleware/auth');
+const { tokenExtractor, userExtractor } = require('../middleware/auth');
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
-const { JWT_SECRET_KEY } = require('./../config/config');
 
 blogRouter.use(tokenExtractor);
+blogRouter.use(userExtractor);
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {
     username: 1,
@@ -32,14 +31,15 @@ blogRouter.get('/:id', async (request, response) => {
 
 blogRouter.delete('/:id', async (request, response) => {
   const token = request.token;
+  const user = request.user;
+
   if (!token) {
     return response.status(401).json({ error: 'login and try again' });
   }
-
-  const user = jwt.verify(token, JWT_SECRET_KEY);
   const blogId = request.params.id;
   const blog = await Blog.findById(blogId);
-  if (blog.user.toString() === user.userId.toString()) {
+  if (!blog) return response.status(404).json({ error: 'blog not found' });
+  if (blog.user.toString() === user._id.toString()) {
     await Blog.findByIdAndDelete(blogId);
     return response.status(204).end();
   }
@@ -50,39 +50,49 @@ blogRouter.delete('/:id', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
   const token = request.token;
+  const requser = request.user;
+  console.log(`${requser} is the user`);
   if (!token) {
     return response
       .status(403)
       .json({ error: 'unauthorized, login and try again' });
   }
-  const decodedToken = jwt.verify(token, JWT_SECRET_KEY);
-  const userId = decodedToken.userId;
   const body = request.body;
   const blog = new Blog({
     author: body.author,
     title: body.title,
     url: body.url,
-    user: userId
+    user: requser._id
   });
   await blog.save();
-  const user = await User.findById(userId);
+  const user = await User.findById(requser._id);
   user.blogs = user.blogs.concat(blog._id);
   await user.save();
   return response.status(201).json(blog);
 });
 
 blogRouter.put('/:id', async (request, response) => {
-  const { id } = request.params;
-  const { likes, title, url } = request.body;
+  const token = request.token;
+  const user = request.user;
+  const { likes, title, url, author } = request.body;
+  const blogId = request.params.id;
+
+  if (!token) {
+    return response.status(401).json({ error: 'login and try again' });
+  }
+  const blog = await Blog.findById(blogId);
+  if (!blog) return response.status(404).json({ error: 'blog not found' });
+  if (blog.user.toString() !== user._id.toString()) {
+    return response
+      .status(403)
+      .json({ error: 'not authorized to delete this blog' });
+  }
   const updatedBlog = await Blog.findByIdAndUpdate(
-    id,
-    { likes, title, url },
+    blogId,
+    { likes, title, url, author },
     { new: true }
   );
-  if (updatedBlog) {
-    return response.json(updatedBlog);
-  }
-  return response.status(404).json({ error: 'No blog with such id exits' });
+  return response.json(updatedBlog);
 });
 
 module.exports = blogRouter;
