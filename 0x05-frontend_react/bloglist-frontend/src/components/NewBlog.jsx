368dd868e34568fdfa455a65ed74e82f7cd34284
Itsfoss0 <akhonyaebenezer@gmail.com>
2024-02-11 19:45:56 +0300

    feat: allow creating new blogs

--- 0x05-frontend_react/bloglist-frontend/src/components/NewBlog.jsx
+++ 0x05-frontend_react/bloglist-frontend/src/components/NewBlog.jsx
@@ -1,18 +1,31 @@
+
+import { useState } from 'react';
 import blogService from '../services/blogs';
 
 const NewBlog = () => {
+  const [message, setMessage] = useState(null);
+  const clearMessage = () => {
+    setTimeout(() => {
+      setMessage(null);
+    }, 2000);
+  };
   const submitBlog = async (event) => {
     event.preventDefault();
     const user = JSON.parse(window.localStorage.getItem('user'));
     const data = new FormData(event.target);
     const payload = Object.fromEntries(data);
-    // console.log(payload, user.accessToken)
     const resp = await blogService.createNew(user.accessToken, payload);
-    console.log(resp);
+    if (resp.status === 200) {
+      setMessage('Added New blog');
+      clearMessage();
+      return
+    }
+    setMessage('Could not add blog')
+    clearMessage()
   };
 
   return (
-    <>
+    <>  {message && <div>{message}</div>}
       <h2>Add another blog</h2>
       <form onSubmit={submitBlog} method='POST'>
         <div>Title: <input type='text' name='title' /></div>
--- 0x05-frontend_react/bloglist-frontend/src/services/blogs.js
+++ 0x05-frontend_react/bloglist-frontend/src/services/blogs.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:1337/api/blogs';
+const baseUrl = '/api/blogs';
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
