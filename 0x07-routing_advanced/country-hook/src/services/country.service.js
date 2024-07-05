8377d8fa32ef99b9c41703aeae4e501901024a02
Itsfoss0 <akhonyaebenezer@gmail.com>
2024-07-06 00:03:29 +0300

    [fix]: fix country fetching

--- 0x07-routing_advanced/country-hook/src/services/country.service.js
+++ 0x07-routing_advanced/country-hook/src/services/country.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/';
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/name';
 
 export const getCountryData = async (country) => {
   const resp = await axios.get(`${baseUrl}/${country}`);
